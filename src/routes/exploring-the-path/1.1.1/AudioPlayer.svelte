41f449228fbc9012836a57cbf02f94a114543583
Steve Hanlon <rshanlon@gmail.com>
2022-02-20 20:54:17 -0800

    Added functionality to hide audio player; added fixed audio icon to show the player; adjusted CSS

--- src/routes/exploring-the-path/1.1.1/AudioPlayer.svelte
+++ src/routes/exploring-the-path/1.1.1/AudioPlayer.svelte
@@ -2,24 +2,36 @@
 	import { fly } from 'svelte/transition';
   let y = 0;
 	
-	export let audioLink;
 	let playerHidden = false;
-// $: console.log(playerHidden)
+	$: console.log(playerHidden)
+	
+	const togglePlayer = () => {
+		return playerHidden ? playerHidden = false : playerHidden = true
+	}
 </script>
 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <svelte:window bind:scrollY={y} />
 
-<section class:fixed={y >= 250}				 
-				 transition:fly={{ y: 200, duration: 500 }}>	
-	
-	<audio controls class:hide={playerHidden}>
-		<source src={audioLink} type="audio/mp3">
-	Your browser does not support the audio element.
-	</audio>
-	
-	<span class="close" on:click={() => playerHidden != playerHidden}>&times;</span>
-</section>	
+{#if !playerHidden}
+	<section class:fixed={y >= 250} 
+					 transition:fly={{ y: 200, duration: 500 }}>
+
+			<audio controls>
+				<source src="https://learning.pariyatti.org/pluginfile.php/183/mod_page/content/4/audio/01%201.1.1%20Bahujanahitasutta.mp3" type="audio/mp3">
+			Your browser does not support the audio element.
+			</audio>
+
+		<div class="close" on:click={togglePlayer}>&times;</div>
+	</section>	
+{:else}
+	<div id="audio-icon" 
+			 transition:fly={{ y: 50, duration: 500 }}
+			 on:click={togglePlayer}>
+		<i class="fa fa-volume-up" aria-hidden="true"></i>
+	</div>
+{/if}
 
 
 <style>
@@ -31,6 +43,7 @@
 		justify-content: center;
 		align-items: start;
 		margin: 10px 0;
+		transition: all 1s;
 	}
 	
 	audio {
@@ -39,17 +52,25 @@
 		background: black;
 	}
 	
-		audio:before {
-		content: "X"
+	#audio-icon {
+		position: fixed;
+		font-size: 1.3rem;
+		bottom: 0;
+		right: 0;
+		background: hsl(41, 29%, 85%);
+		border: 1px solid black;
+		border-radius: 50%;
+		padding: 3px 5px 1px;
+		cursor: pointer;
 	}
 	
 	.fixed {
 		top: 0;
 	}
 	
-	.hide {
-		transform: translateX(200px)
-	}
+/* 	.hide {
+		transform: translateX(-40%)
+	} */
 	
 	.close {
 		color: white;
@@ -59,11 +80,14 @@
 		font-weight: bold;
 	}
 
-	.close:hover,
-	.close:focus {
+	.close:hover {
 		color: red;
 		text-decoration: none;
 		cursor: pointer;
+	}
+	
+	.close:active {
+		color: green;
 	}
 
 </style>
