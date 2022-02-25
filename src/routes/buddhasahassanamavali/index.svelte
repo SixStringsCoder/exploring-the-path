ad4750d9fa0017ebac8230e4c2b0e496f10a6642
Steve Hanlon <rshanlon@gmail.com>
2022-02-24 21:12:05 -0800

    Fleshed out filler content for Buddhasahasanamavali home page

--- src/routes/buddhasahassanamavali/index.svelte
+++ src/routes/buddhasahassanamavali/index.svelte
@@ -1,1 +1,76 @@
-<h1>Buddhasahassanāmāvali Course</h1>
+
+
+<main>
+    <h3>Buddhasahassanāmāvali Course</h3>
+    <h3 class="sectionname">
+      Gratitude Verses to the Buddha
+    </h3>
+
+    <div class="summary">
+        <div class="no-overflow">
+            <div style="font-family: Tahoma, sans-serif;">
+                <!--Intro by Klaus Nothnagel-->
+                <h3 style="text-align: center;">Introduction</h3>
+                <p>
+                    Serving at Dhammagiri in the late eighties and early nineties has imprinted itself as the most rewarding experience in my memory. It was then, after the huge crowd of meditators—especially from the West—who had flocked
+                    in for the winter period, had already disappeared, that only about a dozen of us, mostly Pāli-students, used to stay on for longer periods.
+                </p>
+
+                <p>
+                    In those days a lot of Dhamma-service was still being done by westerners. Aside from regular ongoing duties like early morning gong, hall duty, food service and gardening, there were always sudden and emergent odd jobs.
+                    The regular Indian paid staff took care of management, registration, cleaning and kitchen work. ...<br />
+                    <a href="https://learning.pariyatti.org/mod/page/view.php?id=986#introCont">Read More</a>
+                </p>
+
+                <!--Foreword by Goenka-->
+                <hr />
+                <h3 style="text-align: center;">Foreword</h3>
+
+                <p>
+                    It is my great good fortune that I was born and grew up in an extremely devout Indian family settled in Burma. The devotional books of the Gita Press, Gorakhpur, had a great influence on the whole family. My father was a
+                    devotee of Shiva while my mother was a devotee of Sri Krishna. In the first decade of my life, I used to daily chant either the <em>Vishnu Sahastranama</em> or the <em>Gopal Sahastranama </em>or the
+                    <em>Shivamahimna Stotra</em> or the <em>Shivartandav Stotra</em> or some chapters of the <em>Gita</em>. I used to enjoy it. Besides strengthening my devotion, another important benefit was that I learned the proper
+                    pronunciation of Sanskrit and also gained some knowledge of it.
+                </p>
+                <p>
+                    In the second decade of my life, my good fortune increased when I came in contact with the Arya Samaj. The teachings jolted my mind. I began to understand the difference between true devotion and bind devotion, true
+                    faith and blind faith. Instead of blindly accepting everything that I heard or read, I developed the wisdom of examining it and testing whether it was logical and rational. I was indeed fortunate.
+                    <a href="https://learning.pariyatti.org/mod/page/view.php?id=989#forwordCont">Read More</a>
+                </p>
+
+                <hr />
+            </div>
+        </div>
+    </div>
+
+    <ul class="section img-text">
+        <li class="activity page modtype_page" id="module-986">
+            <div>
+                <div class="mod-indent-outer">
+                    <div class="mod-indent"></div>
+                    <div>
+                        <div class="activityinstance">
+                            <a href="https://learning.pariyatti.org/mod/page/view.php?id=986">
+                                <span class="instancename">Introduction by Klaus Nothnagel<span class="accesshide"> Page</span></span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </li>
+        <li class="activity page modtype_page" id="module-989">
+            <div>
+                <div class="mod-indent-outer">
+                    <div class="mod-indent"></div>
+                    <div>
+                        <div class="activityinstance">
+                            <a href="https://learning.pariyatti.org/mod/page/view.php?id=989">
+                                <span class="instancename">Foreword by S.N. Goenka<span class="accesshide"> Page</span></span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </li>
+    </ul>
+</main>
