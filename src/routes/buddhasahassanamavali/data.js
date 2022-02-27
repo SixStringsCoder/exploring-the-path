4580278ceba03421be1f712e6ffe5941dad88c3e
Steve Hanlon <rshanlon@gmail.com>
2022-02-26 21:34:21 -0800

    Updated Buddhasa data

--- src/routes/buddhasahassanamavali/data.js
+++ src/routes/buddhasahassanamavali/data.js
@@ -31,5 +31,38 @@ export const versesData = [
     quizletID: "191531408",
     audioURL: "https://learning.pariyatti.org/pluginfile.php/5772/mod_page/content/52/verse_1.mp3",
     pdfURL: ""
-  }
+  },
+  {
+    id: 2,  
+    devapali: `
+    पुरिसदम्मसारथी,
+    लोकविदू अनुत्तरो;
+    सत्थादेवमनुस्सानं,
+    बुद्धं तं पणमाम्यहं.
+`,
+    romanpali: `
+    Purisadammasārathī,
+    Lokavidū anuttaro;
+    Satthādevamanussānaṃ,
+    Buddhaṃ taṃ paṇamāmyahaṃ.
+
+`,
+    engtrans: `
+    I bow to the Buddha,
+    Trainer of men,
+    Incomparable knower of all the worlds,
+    Teacher of gods and men alike.  
+`,
+    vocabulary: [ 
+      { word: "purisadammasārathī", definition: "trainer of men (purisa+damma+sārathī men+trainer+charioteer)" },
+      { word: "lokavidū anuttaro", definition: "incomparable knower of all the worlds (loka+vidū+anuttaro world/universe+knowing+incomparable/unrivalled)" },
+      { word: "vsatthā devamanussānaṃ", definition: "teacher of gods and men alike" },
+    ],
+    quizletID: "193190108",
+    audioURL: "https://learning.pariyatti.org/pluginfile.php/5807/mod_page/content/8/verse_2.mp3",
+    pdfURL: ""
+  },
 ]
+
+
+
