c3598912a4768822580a44cb4120b238dcfa3f8c
Steve Hanlon <rshanlon@gmail.com>
2022-05-24 21:49:43 -0700

    Added new footnotes

--- src/routes/exploring-the-path/1.1.1/Introduction.svelte
+++ src/routes/exploring-the-path/1.1.1/Introduction.svelte
@@ -153,106 +153,33 @@ bhagavā.<sup>11</sup> So dhammaṃ deseti ādikalyāṇaṃ majjhekalyāṇaṃ
 <div class="footnotes">
   <ol>
     <hl>Notes</hl>
-    <li id="pali-note1">All the suttas starting with <span class="pali">vuttañhetaṃ</span>—this has been said—refer to the female slave of Queen Samāvāti by the name <span class="pali">Khujjhuttarā</span> in Kosambī. The name Khujjhuttarā was given to her because she had been born with a deformed back (<span class="pali">khujja</span>- bent). It is said that in one of her past lives she had imitated a Pacceka Buddha, who was a hunchback. Despite her position as a slave-girl (because in another previous life she had treated a nun like her servant-girl) she had the capacity of quick understanding and the ability to memorize whatever she heard. This was a result of a situation when she proved fast thinking in a previous life, where she quickly presented bracelets to Pacceka Buddhas, who had trouble holding extremely hot food in their bowls). Her main duty under Queen Samāvāti had been to procure a fresh bouquet of flowers everyday. It had been her habit to buy only half a bunch of flowers and to keep the other half of money for herself. This attitude changed when she listened to a discourse of the Buddha one day, which inspired her to present the correct amount of flowers to the queen. The queen was surprised by the large amount of beautiful flowers on that day and was accordingly informed about the past bad habit. But instead of rebuking Kujjhuttarā, the Queen Samāvāti instructed her with a new task: From hence on she had to go and listen to the discourses of the Buddha and to repeat them to her every day. The collection of these suttas constitutes the <span class="pali">Itivuttakapāḷi</span>. <a href="#pali-word1">BACK</a></li>
-    <li>The suffix: <span class="pali">-māna </span> forms like the alternate rare form: āna the pres.p. of the medium voice.</li>
-    <li>After the Buddha had ordained his first five disciples, the noble youth Yasa and his friends, the
-number of Arahants had grown up to sixty-one. Before the first rains retreat the Buddha stirred them
-with the following words to spread the Dhamma : ‘Caratha, bhikkhave, cārikaṃ bahujanahitāya
-bahujanasukhāya lokānukampāya atthāya hitāya sukhāya devamanussānaṃ; mā ekena dve agamittha; desetha,
-bhikkhave, dhammaṃ ādikalyāṇaṃ majjhekalyāṇaṃ pariyosānakalyāṇaṃ sātthaṃ sabyañjanaṃ
-kevalaparipuṇṇaṃ parisuddhaṃ brahmacariyaṃ pakāsetha.’—“Wander forth, Bhikkhus, for the wellbeing of
-many, for the happiness of many, out of compassion for the world, for the benefit, the welfare and
-happiness of gods and men. No two should wander together the same direction. Proclaim, Bhikkhus, the
-Dhamma which is beneficial in the beginning, beneficial in the middle, and beneficial in the end, with
-correct wording full of meaning, complete in itself, utterly pure and displaying the holy life.” (see
-Vinaya, Mahāvaggo)</li>
-<li>lokānukampāya: lit.:trembling along with, i.e. having compassion, out of pity for (the suffering
-in) the world</li>
-<li>bhikkhave, here untranslated as :– ‘oh Bhikkhus’—is generally the term the Buddha used to
-address his assembly, which often consisted not only of monks and nuns, but of laypeople as well.</li>
-<li>The expression tathāgato is a term that the Buddha uses whenever he talks about himself. It can
-be derived from tathā gata ( having gone thus. i.e. having gone the path to enlightenment from the
-beginning to the end ) as well as from tatha āgata ( having come thus. i.e. having attained the state of
-enlightenment by the path he and all the Buddhas have proclaimed). For more details of the qualities of tathāgato refer to Lokasuttaṃ and Kāḷakārāmasuttaṃ in Aṅguttaranikāyo, CatukkanipātapāḷI,
-Paṭhamapaṇṇāsakaṃ, Uruvelavaggo.</li>
-<li>The following qualities of the Buddha usually start with “itipiso bhagavato arahato
-sammāsambuddho…” and illustrate and frame together with the qualities of Dhamma “svakkhato bhagavato
-dhammo…” and the qualities of Sangha “supattipanno bhagavato savaka sangho…” the homage “vandana” to
-the Triple Gem. The inspiration derived for one's own practice out of properly understanding these
-qualities is called Buddhanusati, Dhammanusati and Sanghanusati. These ‘-anusati’ are explained in great
-detail in the Visudhimaggo.</li>
-<li>The chapter of Buddhanusati in the Visudhimaggo deals in great detail with the deriviation of
-roots and meanings of each of these epitepths. For example the word arahat can be derived in various
-ways. Araha means—deserving, worthy-: arahat is someone worthy of one’s veneration. ara also takes the
-connotation of a -spoke of a wheel- and hari describes -an enemy-, hata or hanta are derived from the
-word hanati—to kill, destroy, so the word arahat is understood as: -somebody, who has destroyed these
-spokes (of the wheel of becoming) and killed his enemies.</li>
-<li>The term vijjacarana is translated as theory and practice. A practitioner should understand that
-carana consists of sīla and samadhi and vijja of panna. A simile compares vijja with the the eye of a human
-being while carana resembles the limbs. In the same sense as a person is only perfect with good eyesight
-and fully-functioning limbs, a practitioner of Dhamma should therefore establish oneself in carana by
-developing proper concentration with a base of perfect moral behaviour as well as vijja, developing one’s
-own wisdom through insight.</li>
-<li> Literally sugato means -well gone-, but could also be derived for example from sobhaṇa-gamana
-(his pefect manner of trodding the path), sammā gata (having gone rightly without derivation) etc. For
-another explanation of sugato –well spoken- derived from sammā gadati see sutta 2.3.20.</li>
-<li> Refer to commentarial explanation of bhagavā: for example in: Maṅgalasuttavaṇṇanā,
-Nikkhepappayojanaṃ: Bhaggarāgo bhaggadoso, bhaggamoho anāsavo; Bhaggāssa pāpakā dhammā,
-bhagavā tena vuccatī”ti—Having abolished desire, aversion and delusion, freed from impurities, rid of all
-evil states- such a One is rightly called a Bhagavā.</li>
-<li> Brahmacariyaṃ is here translated as ‘holy life’. One who decides to lead the ‘life of a brahma’ in
-the sense of a bhikkhu who leaves the household and is expected to accomplish the precepts as laid out
-in the Vinaya. For a householder the third precept means full abstention from sexual activities:
-Abrahmacariyā veramaṇī sikkhāpadaṃ samādiyāmi - ‘I undertake the rule of training to live a life of
-chastity’. For examples compare Majjhimanikā, MūlapaṇṇāsapāḷI, Mahāyamakavaggo, Cūḷasāropamasuttaṃ
-and Mahāsāropamasuttaṃ or the Brahmacariyasuttaṃ in: Aṅguttaranikāyo, Catukkanipātapāḷi, Uruvelavaggo.</li>
-<li> khīṇāsavo: an epithet for a person, an Arahant, whose mind is freed from all the four mental
-obsessions, i.e. kāmāsava. bhavāsava, diṭṭhāsava, avijjāsava—desire for sensuality, desire for living,
-attachement to philosophical speculations, and ignorance.</li>
-<li> The expression: sātthaṃ sabyañjanaṃ can be translated purely literal: ‘with—sa- meaning, with
-differentiation’. Attha has a wide variety of meanings (goal, benefit, meaning,…) and byañjana is
-understood as letter, characteristic mark. The common translation is: ‘the spirit and the letter’. The
-translation: ‘with correct wording full of meaning’ tries to convey the sense of the commentarial
-explanations. The commentary relates sātthaṃ to paṭipatti and elucidates that the explanation of meaning
-should be satisfying and inspiring to any wise person through it’s profoundness and purity of
-explanation. Sabyañjanaṃ is related to pariyatti , it should be eliminating all doubts amongst worldly folks
-and generate faith through the profoundness of teaching, analytical exegesis and proper placement of
-words: Yaṃ pana bhagavā dhammaṃ desento sāsanabrahmacariyaṃ maggabrahmacariyañca pakāseti,
-nānānayehi dīpeti, taṃ yathānurūpaṃ atthasampattiyā sātthaṃ, byañjanasampattiyā sabyañjanaṃ. Saṅkāsana,
-pakāsana, vivaraṇa, vibhajana, uttānīkaraṇa paññatti atthapadasamāyogato sātthaṃ,
-akkharapadabyañjanākāraniruttiniddesasampattiyā sabyañjanaṃ, atthagambhīratāpaṭivedhagambhīratāhi vā
-sātthaṃ, dhammagambhīratādesanāgambhīratāhi sabyañjanaṃ. Atthapaṭibhānapaṭisambhidāvisayato vā
-sātthaṃ, dhammaniruttipaṭisambhidāvisayato sabyañjanaṃ. Paṇḍitavedanīyato parikkhakajanappasādakanti
-sātthaṃ, saddheyyato lokiyajanappasādakanti sabyañjanaṃ. Gambhīrādhippāyato sātthaṃ, uttānapadato
-sabyañjanaṃ. Upanetabbassa abhāvato sakalaparipuṇṇabhāvena kevalaparipuṇṇaṃ, apanetabbassa abhāvato
-niddosabhāvena parisuddhaṃ, apica paṭipattiyā adhigamabyattito sātthaṃ, pariyattiyā āgamabyattito
-sabyañjana…… Tasmā “sātthaṃ sabyañjanaṃ …pe… pakāsetī”ti vuccati.
-When the Bhagavā, teaching the Dhamma, declares the holy life of the dispensation and the holy
-life of walking on the path, he throws light on it in manifold ways: if corresponding to the
-comprehension of the meaning: it is sātthaṃ, if corresponding to the comprehension of the letter: it is
-sabyañjanaṃ. Giving explanation, making known, uncovering, going into detail, clarification, description
-with the motto of conjunction: this is sātthaṃ; for the comprehension of letters and words, the
-explanation and analytical exegesis of letters and forms: it is sabyañjanaṃ.
-Because of depth of meaning, profoundness of comprehension: it is sātthaṃ ; because of depth of
-Dhamma and profoundness of teaching: it is sabyañjanaṃ. Because of illumination of meaning, analytical
-knowledge in the sphere of meaning in extension: it is sātthaṃ; because of grammatical analyses in the
-field of casual relation, explanation of the origin of words, it is sabyañjanaṃ. Leading to comprehension
-amongst the wise ones and making it bright to investigating beings: it is sātthaṃ; inspiring faith and
-making it bright to the worldly people: it is sabyañjanaṃ.
-Profound in inference: it is sātthaṃ; clear in words: it is sabyañjanaṃ.
-It should bring to conclusion, not be in want of anything, it should be entire, complete and
-developed, fulfilled in it’s entity, it should remove (faults), not be in want of anything, faultless pure and
-developed, utterly pure and further more through paṭipatti lead to the manifestation of knowledge: it is
-sātthaṃ, through pariyatti it should be for the accomplishment of scriptures: it is
-sabyañjanaṃ……Therefore it is said: “He teaches …… pe …… with correct wording full of meaning.”</li>
-<li> ‘ti: The oral tradition indicates with the syllable : ‘ti’ the end of a quotation, today often shown
-by quotation marks. ‘ti’ therefore remains untranslated. If the preceding word ends in ‘ṃ’ -
-(devamanussānaṃ)- the pronunciation rules demand the change from ‘ṃti’ to the dental: ‘nti’:
-saddhammassavanaṃ + ti’ : saddhammassavananti</li>
-<li> The expression: Etamatthaṃ bhagavā avoca—The Bhagava explained this matter—in general
-concludes the prose part of the words of the Buddha. The following expression: Tatthetaṃ iti vuccati —… in
-connection of which this was stated…- introduces verses that are then added in this connection to
-summarise the meaning in verse.</li>
+		<li id="pali-note1">It was the aspiration that this benevolent wish of S.N.Goenka should be fulfilled which laid the base for the preparation of this Pāli-learning-programme and the short <a href="#">‘Introduction to Pāli Grammer’</a>. <a href="#pali-word1">BACK</a></li>
+    <li>This ‘Exploring the Path in the Direct Words of the Buddha’ (ETP) uses as source the Chatta Saṅghayana Tipiṭaka based on the Burmese version as published by <a href="#">Vipassana Research Institute</a>.</li>
+		<li>The prophesy uttered by Thera Phussa that the current period of the second (half of the) sasana of 2500 years is refered to in more detail at: 3.1.2 Sīsapāvanasuttaṃ - Like a Handful of Leaves </li>
+		<li>see vocabulary of this lesson for translation</li>
+		<li>Mārakathā, Mahākhandhako, Mahāvaggapāḷi, Vinayapiṭake</li>
+		<li>Similar to the opening ‘vuttañhetaṃ’ the expression: ‘etamatthaṃ bhagavā avoca’—‘The Bhagava explained this matter’—in general concludes the prose part of the words of the Buddha in the Itivuttakapāḷi
+	The following expression:  ‘tatthetaṃ iti vuccati’ —‘in connection of which this was stated’— than introduces verses that are added in this connection to summarise the meaning in verse.</li>
+		<li>
+			The expression tathāgato is a term that the Buddha uses whenever he talks about himself. It can be derived from tathā gata ( having gone thus; i.e. having gone the path to enlightenment from the beginning to the end ) as well as from tatha āgata ( having come thus; i.e. having attained the state of enlightenment by the path he and all the Buddhas have proclaimed). For more details of the qualities of tathāgato refer to Lokasuttaṃ and Kāḷakārāmasuttaṃ in Aṅguttaranikāyo, Catukkanipātapāḷi, Paṭhamapaṇṇāsakaṃ, Uruvelavaggo.
+		</li>
+		<li>ehipassiko: ehi (imp. of eti) + passiko: come + seeing: that which invites to come and see (quality of Dhamma) </li>
+		<li>asaṅkheyye: lit. not to be calculated: incalculable, an immense long period of time.</li>
+		<li>
+			kappa: a fixed period of time, of individual as well as of cosmic life: an aeon. A simile compares the period of a kappa not even exhausted after someone was to take out one seed every hundred years from a cube like granary of one yojana in length, breadth and height, filled with tiny mustard seeds. 
+			yojana: this was the traditional measurment of length and distance, usually understood as the distance a yoke could be drawn by an oxen, about seven miles.
+		</li>
+		<li>satasahasse: sata + sahasse: hundred + thousand</li>
+		<li>Sumedhapatthanākathā, Buddhavaṃsapāḷi, Khuddakanikāye</li>
+		<li>In this town, also called Amaravatī, lived the bodhisatta Sumedho: ‘Nagare amaravatiyā, sumedho nāma brāhmaṇo’… From there Sumedha left into solitude into the Himalayas, where he dwelled in a leafhut and dedicated all his time to meditation. There taking notice of the arising of the Sammasambhuddha Dīpaṅkaro (Dīpaṅkaro nāma jino, uppajji lokanāyako) he got thrilled and immediately made his way to pay homage. </li>
+		<li>tiṇṇa: pp. of tarati: cross, overcome</li>
+		<li>thāmadassinā: thāma + dassinā: strength, power+ seeing</li>
+		<li>santāressaṃ: santāressāmi: (fut., caus. of tārati): I will make cross</li>
+		<li>as above</li>
+		<li>paramatthāya: parama + attha (dat.): best, superior + benefit, welfare</li>
+		<li>Bahujanahitasuttavaṇṇanā</li>
+		<li>taṃsampāpakamaggatthāyāti: taṃ + sampāpaka + magga + atthāyāti: this + causing to optain + path + benefit</li>
+		<li>phalasamāpattisukhatthāyāti: phala + samāpatti + sukha +atthāya +ti: fruit +attainment + happiness + for the sake of</li>
   </ol>  
 </div>
 </section>
